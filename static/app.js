97ae9a9b49c783d26d9ddd99c27e064710a86ef7
dammyonanuga <onanugadammy@gmail.com>
2023-06-13 15:52:26 -0500

    Initial commit

---
+++ static/app.js
@@ -0,0 +1,19 @@
+class Chatbox {
+    constructor() {
+        this.args = {
+            openButton: document.querySelector(selectors:'.chatbox__button'),
+            chatBox: document.querySelector(selectors:'.chatbox__support'),
+            sendButton: document.querySelector(selectors:'.send__button')
+            }
+
+            this.state = false;
+            this.messages = [];
+            }
+
+    display() {
+        const {openButton, chatBox, sendButton} = this.args;
+        openButton.addEventListener(type:'click', listener:() => this.toggleState(chatBox))
+        sendButton.addEventListener(type: 'click', listener:() => this.onSendButton(chatBox))
+        const node = chatBox.querySelector()
+        }
+}
